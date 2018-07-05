d2d9918d57a02e33b48aa0123b7b743c1c089d04
vagrant <vagrant@kleer-csd-box>
2018-07-05 15:32:13 -0300

    letra fija

--- lib/ahorkado.rb
+++ lib/ahorkado.rb
@@ -1,10 +1,15 @@
 class Ahorkado
 	def initialize
 	@palabra = "Prueba"
+	@letra = "P"
 	end
 
 	def obtener_palabra
 	return @palabra
+	end
+
+	def obtener_letra
+	return @letra
 	end
 
 end
--- spec/palabrafija_spec.rb
+++ spec/palabrafija_spec.rb
@@ -6,5 +6,9 @@ it "palabrafija" do
 	ahorkado = Ahorkado.new
 	ahorkado.obtener_palabra.should == "Prueba"
 end
+it "letrafija" do
+	ahorkado = Ahorkado.new
+	ahorkado.obtener_letra.should == "P"
+end
 end
 
